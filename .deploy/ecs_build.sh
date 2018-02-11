cb4ae088b2072ca0c1a30f488b0b1a70ba96e995
Sushil Shinde <shinde.sushil@gmail.com>
2018-02-11 12:32:50 +0530

    Script updated

--- .deploy/ecs_build.sh
+++ .deploy/ecs_build.sh
@@ -59,8 +59,137 @@ push_ecr_image() {
 	echo "Docker Image published."
 }
 
+register_definition() {
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        return 1
+    fi
+
+}
+
+
+check_service_status() {
+        counter=0
+	sleep 60
+        servicestatus=`aws ecs describe-services --service $AWS_ECS_SERVICE --cluster $AWS_ECS_CLUSTER | $JQ '.services[].events[0].message'`
+        while [[ $servicestatus != *"steady state"* ]]
+        do
+           echo "Current event message : $servicestatus"
+           echo "Waiting for 30 sec to check the service status...."
+           sleep 30
+           servicestatus=`aws ecs describe-services --service $AWS_ECS_SERVICE --cluster $AWS_ECS_CLUSTER | $JQ '.services[].events[0].message'`
+           counter=`expr $counter + 1`
+           if [[ $counter -gt $COUNTER_LIMIT ]] ; then
+                echo "Service does not reach steady state with in 10 minutes. Please check"
+                exit 1
+           fi
+        done
+        echo "$servicestatus"
+}
+
+make_task_def(){
+	task_template='[
+		{
+		"name": "%s",
+		"image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
+		"essential": true,
+		"memory": 500,
+		"cpu": 100,
+		"environment": [
+        {
+          "name": "AUTH_DOMAIN",
+          "value": "%s%"
+        },       
+        {
+          "name": "OLTP_PW",
+          "value": "%s%"
+        },
+        {
+          "name": "OLTP_URL",
+          "value": "%s%"
+        },
+        {
+          "name": "OLTP_USER",
+          "value": "%s%"
+        },
+        {
+          "name": "SMTP_HOST",
+          "value": "%s%"
+        },
+        {
+          "name": "SMTP_PASSWORD",
+          "value": "%s%"
+        },
+        {
+          "name": "SMTP_SENDER",
+          "value": "%s%"
+        },
+        {
+          "name": "SMTP_USERNAME",
+          "value": "%s%"
+        },
+        {
+          "name": "TC_JWT_KEY",
+          "value": "%s%"
+        }
+      ],
+		"portMappings": [
+        {
+          "hostPort": 8080,
+          "protocol": "tcp",
+          "containerPort": 8080
+        },
+        {
+          "hostPort": 8081,
+          "protocol": "tcp",
+          "containerPort": 8081
+        }
+      ],
+		"logConfiguration": {
+			"logDriver": "awslogs",
+				"options": {
+							"awslogs-group": "/ecs/%s",
+							"awslogs-region": "%s",
+							"awslogs-stream-prefix": "ecs"
+				}
+			}
+		}
+	]'
+	
+	task_def=$(printf $task_template $AWS_ECS_CONTAINER_NAME $AWS_ACCOUNT_ID $AWS_REGION $AWS_REPOSITORY $TAG $AUTH_DOMAIN $OLTP_PW $OLTP_URL $OLTP_URL $OLTP_USER $SMTP_HOST $SMTP_PASSWORD $SMTP_SENDER $SMTP_USERNAME $TC_JWT_KEY $AWS_ECS_CLUSTER $AWS_REGION)
+}
+
+
+register_definition() {
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        return 1
+    fi
+}
+
+deploy_cluster(){
+    make_task_def
+    register_definition
+    update_result=$(aws ecs update-service --cluster $AWS_ECS_CLUSTER --service $AWS_ECS_SERVICE --task-definition $revision )
+    result=$(echo $update_result | $JQ '.service.taskDefinition' )
+    echo $result
+    if [[ $result != $revision ]]; then
+        echo "Error updating service."
+        return 1
+    fi
+
+    echo "Update service intialised successfully for deployment"
+    return 0
+}
+
+
 configure_aws_cli
 build_ecr_image
 push_ecr_image
-
+deploy_cluster
+#check_service_status
 exit $?
