38c9c28085ac6a2580b2960a4cce15bcc20c5599
Sushil Shinde <shinde.sushil@gmail.com>
2018-02-21 16:51:15 +0530

    ecs deploy inline cleanup

--- .deploy/ecs_build.sh
+++ .deploy/ecs_build.sh
@@ -41,6 +41,7 @@ cd $DEPLOY_DIR/docker
 echo "Copying deployment files to docker folder"
 cp $WORKSPACE/target/elasticsearch-feeder-service*.jar elasticsearch-feeder-service.jar
 cp $WORKSPACE/src/main/resources/elasticsearch-feeder-service.yaml elasticsearch-feeder-service.yaml
+cp $WORKSPACE/.deploy/ecs_task_template.json ecs_task_template.json
 
 echo "Logging into docker"
 echo "############################"
@@ -51,6 +52,7 @@ docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
 #echo "$ENV after case conversion"
 
 configure_aws_cli() {
+  echo "Configuring AWS CLI."
 	aws --version
 	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
@@ -60,139 +62,37 @@ configure_aws_cli() {
 }
 
 build_ecr_image() {
-	eval $(aws ecr get-login  --region $AWS_REGION)
-	# Builds Docker image of the app.
-	#$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
+  echo "Building docker image..."
+ 	eval $(aws ecr get-login  --region $AWS_REGION)
 	TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
 	docker build -t $TAG .
+  echo "Docker image built with the TAG :"
+  echo $TAG
+
 }
 
 push_ecr_image() {	
-	echo "Pushing Docker Image...."
-	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+  echo "Pushing docker image to ECR..."
+ 	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 	echo $TAG
 	docker push $TAG
-	echo "Docker Image published."
+ echo "Docker image published to ECR"
 }
 
-register_definition() {
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
-        echo "Revision: $revision"
-    else
-        echo "Failed to register task definition"
-        return 1
-    fi
-
-}
-
-
-check_service_status() {
-        counter=0
-	sleep 60
-        servicestatus=`aws ecs describe-services --service $AWS_ECS_SERVICE --cluster $AWS_ECS_CLUSTER | $JQ '.services[].events[0].message'`
-        while [[ $servicestatus != *"steady state"* ]]
-        do
-           echo "Current event message : $servicestatus"
-           echo "Waiting for 30 sec to check the service status...."
-           sleep 30
-           servicestatus=`aws ecs describe-services --service $AWS_ECS_SERVICE --cluster $AWS_ECS_CLUSTER | $JQ '.services[].events[0].message'`
-           counter=`expr $counter + 1`
-           if [[ $counter -gt $COUNTER_LIMIT ]] ; then
-                echo "Service does not reach steady state with in 10 minutes. Please check"
-                exit 1
-           fi
-        done
-        echo "$servicestatus"
-}
 
 make_task_def(){
-task_template=$(cat <<-END
-{
-  "executionRoleArn": "arn:aws:iam::811668436784:role/ecsTaskExecutionRole",
-  "containerDefinitions": [
-    { 
-      "logConfiguration": {
-        "logDriver": "awslogs",
-        "options": {
-          "awslogs-group": "/ecs/tc-elasticsearch-feeder-service",
-          "awslogs-region": "us-east-1",
-          "awslogs-stream-prefix": "ecs"
-        }
-      },     
-      "portMappings": [
-        {
-          "hostPort": 8080,
-          "protocol": "tcp",
-          "containerPort": 8080
-        },
-        {
-          "hostPort": 8081,
-          "protocol": "tcp",
-          "containerPort": 8081
-        }
-      ],      
-      "cpu": 1,
-      "environment": [
-        {
-          "name": "AUTH_DOMAIN",
-          "value": "%s"
-        },
-        {
-          "name": "AWS_SIGNING_ENABLED",
-          "value": "%s"
-        },
-        {
-          "name": "CHALLENGES_INDEX_NAME",
-          "value": "%s"
-        },
-        {
-          "name": "ELASTIC_SEARCH_URL",
-          "value": "%s"
-        },
-        {
-          "name": "OLTP_PW",
-          "value": "%s"
-        },
-        {
-          "name": "OLTP_URL",
-          "value": "%s"
-        },
-        {
-          "name": "OLTP_USER",
-          "value": "%s"
-        },
-        {
-          "name": "TC_JWT_KEY",
-          "value": "%s"
-        }
-      ],      
-      "memoryReservation": 512,      
-      "image": "%s",      
-      "essential": true,      
-      "name": "tc-elasticsearch-feeder-service"
-    }
-  ],
-  "memory": "4096",
-  "taskRoleArn": "arn:aws:iam::811668436784:role/ecsTaskExecutionRole",  
-  "family": "tc-elasticsearch-feeder-service",  
-  "requiresCompatibilities": [
-    "FARGATE"
-  ],
-  "networkMode": "awsvpc",
-  "cpu": "2048"
-}
-END
-)
-    
+  echo "Creating ECS task definition..."  
+  task_template=`cat ecs_task_template.json`
   task_def=$(printf "$task_template" "$AUTH_DOMAIN" $AWS_SIGNING_ENABLED $CHALLENGES_INDEX_NAME $ELASTIC_SEARCH_URL $OLTP_PW "$OLTP_URL" $OLTP_USER $TC_JWT_KEY $TAG)  
+  echo $task_def > task_def.json
+  echo "ECS task definition is created : "
   echo $task_def
-  echo $task_def > config.json
 }
 
 register_definition() {
-    echo "register definition"
-    echo aws ecs register-task-definition --cli-input-json file://config.json --family $family
-    if revision=$(aws ecs register-task-definition  --cli-input-json file://config.json --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+    echo "Registering  ECS task definition..."
+    echo aws ecs register-task-definition --cli-input-json file://task_def.json --family $family
+    if revision=$(aws ecs register-task-definition  --cli-input-json file://task_def.json --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
         echo "Revision: $revision"
     else
         echo "Failed to register task definition"
@@ -201,8 +101,9 @@ register_definition() {
 }
 
 deploy_cluster(){
-  echo "Revision"
+    echo "Revision"
     echo $revision
+    echo "Updating ECS Service..."
     make_task_def
     register_definition
     update_result=$(aws ecs update-service --cluster $AWS_ECS_CLUSTER --service $AWS_ECS_SERVICE --task-definition $revision )
