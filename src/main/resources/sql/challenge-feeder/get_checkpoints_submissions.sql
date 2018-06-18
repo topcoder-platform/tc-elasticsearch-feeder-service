aa19f1591ecc432d88475d5a244adfafe0a43d5f
Guiqiang Zhang <skyhit@gmail.com>
2018-06-18 23:38:20 +0800

    fix for numberOfCheckpointSubmissions field

--- src/main/resources/sql/challenge-feeder/get_checkpoints_submissions.sql
+++ src/main/resources/sql/challenge-feeder/get_checkpoints_submissions.sql
@@ -3,7 +3,7 @@ SELECT
        count(s.submission_id) as numberOfSubmissions
    FROM
        submission s LEFT JOIN upload u on u.upload_id = s.upload_id
-   WHERE s.submission_status_id <> 5
+   WHERE s.submission_status_id in (1, 4)
         AND s.submission_type_id = 3
         AND u.upload_type_id = 1
         AND u.upload_status_id = 1 
