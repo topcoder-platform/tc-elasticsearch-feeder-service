cac1b21dd3aecb1712aa3d8ae3d8774ffcfd9fde
skyhit <skyhit@gmail.com>
2018-06-20 09:52:50 +0800

    fix ordering problem of submissions, registrants and terms (#72)

--- src/main/resources/sql/challenge-feeder/detail/get_registrants.sql
+++ src/main/resources/sql/challenge-feeder/detail/get_registrants.sql
@@ -1,16 +1,27 @@
-SELECT
-    r.project_id AS challengeId,
-    registrationDate.value AS registrationDateStr,
-    reliability.value::DECIMAL AS reliability,
-    handle.value AS handle,
-    decode(rating.value, 'N/A', '0', rating.value)::DECIMAL rating,
-    (SELECT MAX(u.create_date) FROM upload u WHERE u.project_id = r.project_id
-        AND u.resource_id = r.resource_id AND u.upload_type_id = 1
-        AND u.upload_status_id = 1) AS submissionDate
-  FROM resource r
-    INNER JOIN resource_info registrationDate ON registrationDate.resource_id = r.resource_id AND registrationDate.resource_info_type_id = 6
-    INNER JOIN resource_info handle ON handle.resource_id = r.resource_id AND handle.resource_info_type_id = 2
-    LEFT JOIN resource_info reliability ON reliability.resource_id = r.resource_id AND reliability.resource_info_type_id = 5
-    LEFT JOIN resource_info rating ON rating.resource_id = r.resource_id AND rating.resource_info_type_id = 4
-  WHERE
-   r.resource_role_id = 1 AND {filter}
+select
+   u.handle AS handle,
+   rur.create_date AS registrationDate,
+   (select max(s.create_date) from submission s, upload u
+                 where u.project_id = p.project_id and s.upload_id = u.upload_id
+                 and upload_status_id !=2 and s.submission_status_id != 5
+                 and u.resource_id = rur.resource_id) AS submissionDate,
+   decode(ri4.value, 'N/A', '0', ri4.value)::int AS rating,
+   ri5.value::int AS reliability,
+   p.project_id AS challengeId
+  from resource rur
+     , resource_info ri1
+     , project p
+     , user u
+     , outer resource_info ri4
+     , outer resource_info ri5
+ where
+   p.project_id = rur.project_id
+  and rur.resource_id = ri1.resource_id
+  and rur.resource_role_id = 1
+  and ri1.resource_info_type_id = 1
+  and ri4.resource_id = rur.resource_id
+  and ri4.resource_info_type_id = 4
+  and ri5.resource_id = rur.resource_id
+  and ri5.resource_info_type_id = 5
+  and ri1.value = u.user_id
+  and {filter}
