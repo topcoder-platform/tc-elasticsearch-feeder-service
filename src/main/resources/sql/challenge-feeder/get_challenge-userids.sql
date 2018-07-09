c1750cebe9aa87dc81f632d87105cc9e7bb1b598
Guiqiang Zhang <skyhit@gmail.com>
2018-07-09 16:02:08 +0800

    fix retrieve of user Ids

--- src/main/resources/sql/challenge-feeder/get_challenge-userids.sql
+++ src/main/resources/sql/challenge-feeder/get_challenge-userids.sql
@@ -3,4 +3,4 @@ SELECT distinct
          r.user_id AS userId
   FROM RESOURCE r
   INNER JOIN project p ON p.project_id = r.project_id
-  WHERE p.project_category_id NOT IN (27,37) AND {filter}
+  WHERE 1 = 1 AND {filter}
