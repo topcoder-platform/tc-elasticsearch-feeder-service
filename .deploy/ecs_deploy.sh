8f69992afcdff7622181ba91ae229177ac425f61
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2018-07-23 16:39:40 +0530

    Update ecs_deploy.sh

--- .deploy/ecs_deploy.sh
+++ .deploy/ecs_deploy.sh
@@ -45,7 +45,8 @@ cp $WORKSPACE/.deploy/ecs_task_template.json ecs_task_template.json
 
 echo "Logging into docker"
 echo "############################"
-docker login $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
+#docker login $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWD
 
 #Converting environment varibale as lower case for build purpose
 #ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
