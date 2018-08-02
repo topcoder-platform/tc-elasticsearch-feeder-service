203997a27598eb6c7205133970632133475885f8
Deddy Syefria <dsyefria@gmail.com>
2018-08-02 13:19:17 +0700

    Legacy MM final score and rank update

--- src/main/resources/sql/mm-feeder-into-challenges/get_marathon_match_standing.sql
+++ src/main/resources/sql/mm-feeder-into-challenges/get_marathon_match_standing.sql
@@ -1,6 +1,8 @@
 SELECT rr.round_id as challengeId
      , rr.coder_id as userId
      , rr.point_total as provisionalScore
+     , rr.placed as finalRank
+     , rr.system_point_total as finalScore
   FROM informixoltp\:long_comp_result rr
  WHERE rr.attended = 'Y'
    AND {filter}
